4d715fc10c26fe3f7e437c6a18032e487fce3abe
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-04-28 10:39:49 +0700

    Sắp xếp theo tiu chí

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -2,13 +2,22 @@ import { Request, Response } from 'express';
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+    // find 
     const find = {
         deleted: false
     };
     if(req.query.status){
         find["status"] = req.query.status
     }
-    const tasks = await Task.find(find);
+
+    // sort
+    const sort ={}
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toLocaleString();
+        sort[sortKey]=req.query.sortValue
+    }
+    const tasks = await Task.find(find)
+        .sort(sort);
     res.json(tasks);
 }
 
