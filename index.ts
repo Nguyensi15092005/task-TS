d9baa9e071ad6e290906bb350eff90c357ae5416
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-05-01 10:41:46 +0700

    Cài thư viện CORS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
 import * as database from "./config/database";
 import mainV1Routes from './api/v1/routes/index.route';
 import bodyParser from 'body-parser';
@@ -11,8 +12,19 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
+// cors
+// var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
+
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 mainV1Routes(app);
 
