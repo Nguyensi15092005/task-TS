7237d06aeedfbe0ebc9fe4e9fc08be6462e7ec97
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-05-02 16:15:40 +0700

    đặng nhập

--- api/v1/controller/user.controller.ts
+++ api/v1/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import md5 from "md5";
 import User from "../models/user.model";
 import { generateRandomString } from "../../../helper/generate";
+
 // [POST] api/v1/users/register
 export const register = async (req: Request, res: Response) =>{
     try {
@@ -37,6 +38,50 @@ export const register = async (req: Request, res: Response) =>{
         res.json({
             code: 404,
             message: "Đăng ký thất bại!"
+        });
+    }
+}
+
+// [POST] api/v1/users/login
+export const login = async (req: Request, res: Response) =>{
+    try {
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+        const user = await User.findOne({
+            email: email,
+            deleted: false
+        });
+
+        if(!user){
+            res.json({
+                code: 400,
+                message: "Email không tồn tại!"
+            }); 
+            return;
+        }
+        else if(md5(password) !== user.password){
+            res.json({
+                code: 400,
+                message: "Sai mật khẩu!"
+            }); 
+            return;
+        }
+        else{
+            req.body.token = generateRandomString(30);
+            const token = user.token;
+
+            res.cookie("token", token);
+
+            res.json({
+                code: 200,
+                message: "Đăng nhập thành công!",
+                token:token
+            });
+        }
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Đăng nhập thất bại!"
         });
     }
 }
--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import * as controller from "../controller/user.controller";
 const router: Router = Router();
 
-
-
 router.post("/register", controller.register);
+
+router.post("/login", controller.login);
+
 
 export default router;
